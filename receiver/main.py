c428b5d946c2879dae09ff7808373f149710d801
Nekketsu-GIT <Github:*1997Gunners05Software31*>
2021-11-25 19:29:01 +0100

    add shared_key

--- receiver/main.py
+++ receiver/main.py
@@ -5,11 +5,18 @@ import cryptocode
 
 app = Flask(__name__)
 
+def read_key():
+    f = open("../shared_photons/key.txt", "r")
+    key = f.read()
+    return key
+
 @app.route('/', methods=['GET'])
 def index():
-    data = cryptocode.decrypt(request.args.get('data'), '123')
-    print(data)
-    return render_template('index.html', data = data)
+    data = request.args.get('data')
+    key = read_key()
+    data_decoded = cryptocode.decrypt(data, key)
+    print(data_decoded)
+    return render_template('index.html', data = data_decoded)
 
 
 if __name__ == '__main__':
--- sender/main.py
+++ sender/main.py
@@ -22,6 +22,7 @@
 from flask import render_template, Flask, request, redirect
 import cryptocode
 import requests
+import quantumrandom
 
 app = Flask(__name__)
 
@@ -30,13 +31,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
+def share_key(key):
+    f = open("../shared_photons/key.txt", "w")
+    f.write(key)
+    f.close()
 
 @app.route('/crypte', methods=['POST'])
 def crypte():
     data = request.form['data']
-    print(data)
-    encoded_data = cryptocode.encrypt(data, '123')
+    key = quantumrandom.get_data(data_type='hex16', array_length=1, block_size=10)
+    share_key(key[0])
+    encoded_data = cryptocode.encrypt(data, key[0])
+    print(key[0])
 
     return redirect('http://127.0.0.1:5001/?data='+encoded_data)
 
